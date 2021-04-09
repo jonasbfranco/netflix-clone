32250082d7a4fd02fef8dac39d3009889c69be3f
jonasbfranco <jonasbfranco@hotmail.com>
2021-04-09 17:26:59 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect (() => {
+    const loadAll = async () => {
+      //Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+    loadAll();
+  }, []);
+
+
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items} />
+          </div>
+        ))}
+      </section>
+    </div>
+  )
+
+}
